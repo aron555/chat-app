1d4251bc577a9480cea74516d62daa27c6d1e658
itmaxxx <dmitrievmaks77@gmail.com>
2022-01-25 18:54:06 +0200

    Use real user in useChat hook and other improvements

--- client/src/hooks/useChat.ts
+++ client/src/hooks/useChat.ts
@@ -1,19 +1,23 @@
 import { useEffect, useRef, useState } from 'react';
 import io, { Socket } from 'socket.io-client';
-import { Message } from '../store/types';
+import { ContentType, Message } from '../store/types.d';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
 
 const SERVER_HOST: string = process.env.REACT_APP_WS_SERVER_HOST || 'http://localhost:7777';
 
 export const useChat = (chatId: string | undefined) => {
   const [messages, setMessages] = useState<Message[]>([]);
-  const user = { id: `user_${Date.now()}` };
+  const user = useSelector((state: RootState) => state.auth);
 
   const socketRef = useRef<Socket | null>(null);
 
   useEffect(() => {
+    if (!user.isLogged || !user?.user) return;
+
     socketRef.current = io(SERVER_HOST);
 
-    socketRef.current.emit('chat:join', { userId: user.id, token: 'token', chatId });
+    socketRef.current.emit('chat:join', { userId: user.user.id, token: 'token', chatId });
 
     // socketRef.current.on('users', (users) => {
     //   setUsers(users);
@@ -28,13 +32,17 @@ export const useChat = (chatId: string | undefined) => {
       // @ts-ignore
       socketRef.current.disconnect();
     };
-  }, [chatId]);
+  }, [chatId, user]);
 
   const sendMessage = (messageText: string) => {
+    if (!user.isLogged || !user?.user) return;
+
     // @ts-ignore
     socketRef.current.emit('message:send', {
-      userId: user.id,
-      message: messageText,
+      userId: user.user.id,
+      chatId: chatId,
+      content: messageText,
+      contentType: ContentType.Text,
     });
   };
 
