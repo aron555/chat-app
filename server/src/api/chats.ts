4e5f110fece2f30b75f76c0df1af5e2e06341f6b
itmaxxx <dmitrievmaks77@gmail.com>
2022-01-28 04:08:39 +0200

    Get user chats by token

---
+++ server/src/api/chats.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { Chat, GetUserChatsResponse } from '../types';
+import { API_URL } from '../index';
+
+export const getUserChatsByToken = async ({ token }: { token: string }): Promise<Chat[]> => {
+  const userChats = await axios.get<GetUserChatsResponse>(API_URL + '/api/users/me/chats', {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const data = userChats.data.data;
+
+  if (!data?.chats) throw new Error();
+
+  const chats = data.chats;
+
+  return chats;
+};
